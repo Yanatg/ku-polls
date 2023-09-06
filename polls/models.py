76747e01b0efa975c1427d70caa04b9e52698ebc
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-09-06 15:26:12 +0700

    edit Question class

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Question(models.Model):
     """ Model for polls questions """
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    pub_date = models.DateTimeField('date published', auto_now_add=False)
     end_date = models.DateTimeField('date ended', default=None, null=True, blank=True)
 
     def __str__(self):
