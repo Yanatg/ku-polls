f13adc9b26c712486765af969f1a5dedeb498d12
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-09-14 21:22:29 +0700

    update models.py, added Vote class

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Question(models.Model):
@@ -29,7 +30,19 @@ class Choice(models.Model):
     """ Model for polls choices """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    # votes = models.IntegerField(default=0)
+
+    @property
+    def votes(self):
+        # count the votes for this choice
+        return self.vote_set.count()
 
     def __str__(self):
         return self.choice_text
+
+
+class Vote(models.Model):
+    """ Record a Vote for a Poll Choice by User """
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
