e89926fdc253c8782fe2c0b3c42ffc75cf7b5016
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-10-29 10:12:45 +0700

    modified url

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-
-from . import views
+from polls.views.index_view import IndexView
+from polls.views.details import DetailView, vote
+from polls.views.results import ResultsView
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', IndexView.as_view(), name='index'),
+    path('<int:pk>/', DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', vote, name='vote'),
 ]
