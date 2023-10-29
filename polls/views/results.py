f14d704dd04683973204b81835748f19a73a2d6a
Yanatchara JERAJA <yanatchara.j@ku.th>
2023-10-29 10:11:02 +0700

    remove unused import

--- polls/views/results.py
+++ polls/views/results.py
@@ -1,10 +1,7 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
-from django.urls import reverse
 from django.views import generic
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-from ..models import Choice, Question, Vote
+from polls.models import Choice, Question, Vote
 from .details import get_available_questions
 
 
